3fae49806c5562df8c6d6b768cedd97ceb3570eb
smitp415 <smitp@usc.edu>
2019-03-30 18:00:35 -0700

    Added own images

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+import pprint
+from os import listdir
 from models import PredictRawVeggies
 
 tester = PredictRawVeggies()
 
 tester.create_model()
 
-res = tester.call_predict(['artichoke_1.jpg', 'bellpeppers_1.jpg', 'asparagus_23.jpg', 'springonion_1.jpg', 'avocado_1.jpg'], 'img')
-print(res)
+files = [f for f in listdir('img')]
+res = tester.call_predict(files, 'img')
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(res)
